1986cc7eb878451decf19d93a911326d2a386e41
Benoit Petit <110.benp@protonmail.com>
2018-12-10 21:54:03 +0100

    fix annotation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,12 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
+     * Page de contact
+     * 
      * @Route("/contact", name="contact")
+     * 
+     * @return render 
      */
     public function index()
     {
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'Contact',
+            'controller_name' => 'Contactez nous',
         ]);
     }
 }
