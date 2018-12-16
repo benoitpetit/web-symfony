29ff7e2f90f5877a02fd3e8b00f9db3e336984ed
Korel <x.korel.x@gmail.com>
2018-12-16 02:38:26 +0100

    PROJET - Finalisation du BACK OFFICE (/backdev)

--- public/assets/script/admin.js
+++ public/assets/script/admin.js
@@ -1,10 +1,4 @@
 
-// document.getElementById('modal-delete-button').innerHTML = "Supprimer";
-// document.getElementsByClassName('action-save').style.backgroundColor="red";
-// "Cette action est irréversible."
-// "Suppression des données"
-// document.getElementsByClassName('action-save').innerHTML = "Ajouter";
-
 $(document).ready(function(){
 
     $('.action-save').html('Ajouter');
