e4d2fce5eda5154c5b7e2638877151c9d4287114
NathanPri <prigent.nathan@gmail.com>
2018-12-18 10:58:10 +0100

    fix boutton back to top

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,1 +1,15 @@
-// Script principal du site 
+// // Script principal du site 
+// var btn = $('#button');
+
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > 300) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:0}, '300');
+// });
