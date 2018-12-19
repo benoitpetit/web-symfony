a05420935fa8faea9a5b79b6f06319e655633ee0
NathanPri <prigent.nathan@gmail.com>
2018-12-19 16:57:15 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/Migrations/Version20181214085539.php
+++ src/Migrations/Version20181214085539.php
@@ -45,6 +45,11 @@ final class Version20181214085539 extends AbstractMigration
         $this->addSql('ALTER TABLE order_line DROP FOREIGN KEY FK_9CE58EE1FCDAEAAA');
         $this->addSql('ALTER TABLE order_line DROP INDEX IDX_9CE58EE1FCDAEAAA');
         $this->addSql('CREATE VIEW vProduct_tshirt AS SELECT r.id rate_id, CAST(r.rate * 100 AS DECIMAL(5,2)) AS taux_tva, p.price_unit_ht, ROUND(p.price_unit_ht * r.rate) as price_tva, p.price_unit_ht + ROUND(p.price_unit_ht * r.rate) as price_unit_ttc, pt.id product_type_id, pt.product_type, g.id genre_id, g.name, c.id color_id, c.color_name, c.color_hexa, l.id logo_id, l.logo_name, l.slug FROM product p INNER JOIN rate r ON p.rate_id_id = r.id	INNER JOIN product_type pt ON p.product_type_id_id = pt.id INNER JOIN gender g ON p.gender_id_id = g.id, color c, logo l WHERE pt.id = 1 ORDER BY g.id, c.id, l.id');
+<<<<<<< Updated upstream
+=======
+        $this->addSql('CREATE VIEW vOrders_tshirt AS SELECT u.id AS user_id, o.id, o.order_register, (SELECT SUM( ROUND((sol.price_unit_ht - COALESCE(promo_unit_ht, 0)) * (1 + sr.rate), 2)) AS price_ttc FROM user su INNER JOIN address sa ON su.address_billing_id_id = sa.id INNER JOIN orders so ON sa.id = so.address_billing_id_id INNER JOIN order_line sol ON so.id = sol.order_id_id INNER JOIN rate sr ON sol.rate_id = sr.id WHERE su.id = u.id AND so.id = o.id ) price_ttc, order_date FROM user u INNER JOIN address a ON u.address_billing_id_id = a.id INNER JOIN orders o ON a.id = o.address_billing_id_id');
+        // $this->addSql('CREATE VIEW vOrderLines_tshirt AS SELECT u.id AS user_id, ol.order_id_id AS order_id, ol.product_type_id, pt.product_type, ol.product_gender_id, g.name as gender_name, ol.product_color_id, c.color_name, ol.product_size_id, s.size as size_name, CONCAT(s.size, ' - ', s.name) AS size_wording, ol.product_logo_id, l.logo_name, ol.quantity, ol.price_total_ttc FROM user u INNER JOIN address a ON (u.address_billing_id_id = a.id) INNER JOIN orders o ON (a.id = o.address_billing_id_id) INNER JOIN order_line ol ON (o.id = ol.order_id_id) INNER JOIN product_type pt ON (ol.product_type_id = pt.id) INNER JOIN gender g ON (ol.product_gender_id = g.id) INNER JOIN color c ON (ol.product_color_id = c.id AND c.par_type_product = \'@tshirt\') INNER JOIN size s ON (ol.product_size_id = s.id AND s.par_type_product = \'@tshirt\') INNER JOIN logo l ON (ol.product_logo_id = l.id AND l.par_type_product = \'@tshirt\'');
+>>>>>>> Stashed changes
         // $this->addSql('CREATE TABLE migration_versions (version VARCHAR(255) NOT NULL)');
     }
 
@@ -79,7 +84,7 @@ final class Version20181214085539 extends AbstractMigration
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP VIEW vProduct_tshirt');
         $this->addSql('DROP VIEW vOrders_tshirt');
-        $this->addSql('DROP VIEW vOrderLine_tshirt');
+        // $this->addSql('DROP VIEW vOrderLines_tshirt');
         // $this->addSql('CREATE TABLE migration_versions');
         // $this->addSql('DROP SCHEMA `devmyshirts`');
     }
